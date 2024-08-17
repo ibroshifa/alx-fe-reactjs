7faf1dabb4a832c358c63bd92e866184beb83a81
ibrahim <ibrahimshifa111@gmail.com>
2024-08-17 15:16:16 +0300

    Apply Inline CSS Styling to React Components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return (
+<footer style={{ backgroundColor: '#333', color: '#fff', padding: '10px', textAlign: 'center' }}>
+  <p>© 2024 City Lovers</p>
+</footer>
+    );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent(){
+    return (
+        <main style={{ backgroundColor: '#f5f5f5', padding: '20px', fontSize: '1.2em', lineHeight: '1.5', textAlign: 'justify' }}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    );
+    
+}
+export default MainContent;
