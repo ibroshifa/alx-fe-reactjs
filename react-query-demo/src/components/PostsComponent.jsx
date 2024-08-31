bcf94906eae76eacb57ec90bbb64fd942da41d2f
ibrahim <ibrahimshifa111@gmail.com>
2024-08-31 16:16:01 +0300

    Advanced Data Handling with React Query

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { useQuery } from 'react-query';
 const PostsComponent = () => {
     const { isLoading, isError, data , fetchPosts } = useQuery('posts', async () => {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      return response.json();
-    });
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        return response.json();
+      }, {
+        cacheTime: 10000, 
+        staleTime: Infinity, 
+        refetchOnWindowFocus: true, 
+        keepPreviousData: true, 
+      });
   
     if (isLoading) return <p>Loading posts...</p>;
   
