1e7c383008f224c37efe776c1fe1270db61ca491
ibrahim <ibrahimshifa111@gmail.com>
2024-09-06 23:40:56 +0300

    Setting Up Tailwind CSS in a New React Project

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -7,5 +7,11 @@ export default {
   plugins: [],
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
+  variants: {
+    extend: {
+      backgroundColor: ['active'], // For example, to extend background color for active state
+      opacity: ['disabled'],        // For opacity when an element is disabled
+    },
+  },
 }
 
